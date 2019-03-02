5963ac98db87ce262a5d71ba5aba6d2d793701d2
Екатерина <liberty_12@mail.ru>
2019-03-03 00:45:24 +0300

    fix open/close popups

--- js/script.js
+++ js/script.js
@@ -1,31 +1,82 @@
-var ESC_KEYCODE = 27;
-
 var contactButton = document.querySelector(".contacts-button");
 var contactPopup = document.querySelector(".contact-popup");
 var contactCloseButton = contactPopup.querySelector(".close-button");
+
+var input = contactPopup.querySelector(".contact-popup-container");
+var name = input.querySelector("#name");
+var email = input.querySelector("#email");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+contactButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  contactPopup.classList.remove("hidden");
+
+  if (storage) {
+    name.value = storage;
+    email.focus();
+  } else {
+    name.focus();
+  }
+});
+
+contactCloseButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  contactPopup.classList.add("hidden");
+  contactPopup.classList.remove("modal-error");
+});
+
+input.addEventListener("submit", function (evt) {
+  if (!name.value || !email.value) {
+    evt.preventDefault();
+    contactPopup.classList.remove("modal-error");
+    contactPopup.offsetWidth = contactPopup.offsetWidth;
+    contactPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", name.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!contactPopup.classList.contains("hidden")) {
+      contactPopup.classList.add("hidden");
+      contactPopup.classList.remove("modal-error");
+    }
+  }
+});
 
 var map = document.querySelector(".map");
 var mapPopup = document.querySelector(".map-popup");
 var mapCloseButton = mapPopup.querySelector(".close-button");
 
-map.addEventListener("click", function(evt) {
+map.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.remove("hidden");
 });
 
-mapCloseButton.addEventListener("click", function(evt) {
+mapCloseButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.add("hidden");
 });
 
-contactButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  contactPopup.classList.remove("hidden");
-});
-
-contactCloseButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  contactPopup.classList.add("hidden");
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!mapPopup.classList.contains("hidden")) {
+      mapPopup.classList.add("hidden");
+    }
+  }
 });
 
 // services
