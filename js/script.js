2788a9b79aee703c97fdb33c732156108b827693
Екатерина <liberty_12@mail.ru>
2019-03-02 19:11:55 +0300

    add service section work

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var mapPopup = document.querySelector(".map-popup");
 var mapCloseButton = mapPopup.querySelector(".close-button");
 
 map.addEventListener("click", function(evt) {
-  // evt.preventDefault();
+  evt.preventDefault();
   mapPopup.classList.remove("hidden");
 });
 
@@ -26,4 +26,25 @@ contactButton.addEventListener("click", function(evt) {
 contactCloseButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   contactPopup.classList.add("hidden");
+});
+
+// services
+
+var servicesList = document.querySelector(".service-list");
+var servicesItem = servicesList.querySelectorAll(".service-item");
+var services = document.querySelectorAll(".service-description-item");
+
+servicesList.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  var target = evt.target;
+  if (target.classList.contains("service-item-link")) {
+    var targetParent = target.parentElement;
+    for (i = 0; i < services.length; i++) {
+      services[i].classList.remove("service-description-show");
+      servicesItem[i].classList.remove("current-service-item");
+    }
+    var className = "." + target.id;
+    document.querySelector(className).classList.add("service-description-show");
+    targetParent.classList.add("current-service-item");
+  }
 });
